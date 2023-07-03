97814c51898a60981ecdd11584b4f3f47249323e
LuisProgra <132025503+LuisProgra@users.noreply.github.com>
2023-07-02 22:39:41 -0600

    pruebas de ejercicio4

--- exercise_4.js
+++ exercise_4.js
@@ -1,12 +1,11 @@
-function mayor(){
-    let mayorn=0;
-    let num1 = parseInt(document.getElementById("numero1").value);
+function mayor(){ //capturar el valor de ambos números enteros
+    let mayorn=0; //si se cumple o no la condición se almacenara en esta variable, sera la propiedad a mostrar
+    let num1 = parseInt(document.getElementById("numero1").value); //almacenar dato entero con parseInt para convertir el string en un entero.
     let num2 = parseInt(document.getElementById("numero2").value);
-    if(num1>num2){
+    if(num1>num2){ 
         mayorn = num1;
     }else{
         mayorn = num2;
     }
     document.getElementById("resultado").value = mayorn;
-
 }
